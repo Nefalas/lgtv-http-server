0d4238329972bef57eb8bb3d66f88d33ccc4ba94
Alexis Lindgren <alexisli@kth.se>
2018-03-12 09:07:49 +0100

    React client

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
 const PORT = 5555;
 
+const fs = require('fs');
+const prompt = require('prompt');
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-
-const Power = require('./api/Power');
-const Netflix = require('./api/Netflix');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
-let configFile;
-try {
-  configFile = require('./config')
-} catch (e) {
-  fs.writeFile("config.json", '{"ip" : "0.0.0.0", "mac" : "00:00:00:00:00:00"}', function(err) {
-    if(err) {
-      return console.log(err);
-    }
-    console.log("Config file created");
-  });
+checkConfig()
+.then((config) => {
+  console.log("Using config:\n" + JSON.stringify(config, null, 2));
+  run();
+})
+.catch((err) => {
+  console.log("Could not create config: " + err);
+});
+
+function run() {
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  app.use(express.static(__dirname));
+  app.use('/api', require('./routes/api/index'));
+  app.listen(PORT);
+  console.log('Server running at http://localhost:' + PORT);
 }
 
-app.use('/', require('./routes/home'));
-app.use('/api', require('./routes/api'));
-
-app.listen(PORT);
-console.log('Server running at http://localhost:' + PORT);
+function checkConfig() {
+  return new Promise((resolve, reject) => {
+    try {
+      let configFile = require('./config');
+      return resolve(configFile);
+    } catch (e) {
+      prompt.start();
+      prompt.get(['ip', 'mac'], (err, res) => {
+        if (err) reject(err);
+        let config = {
+          ip: res.ip,
+          mac: res.mac
+        };
+        fs.writeFile("config.json", JSON.stringify(config), (err) => {
+          if (err) reject(err);
+          console.log("Config file created");
+          return resolve(config);
+        });
+      });
+    }
+  })
+}
